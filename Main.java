317906483f8b6cb33b548aea60612295a3462f13
tsaksham1304 <sakshamt1304@gmail.com>
2026-03-17 18:40:02 +0530

    UC 1 - Application Entry & Welcome Message

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Welcome message
+        System.out.println("=======================================");
+        System.out.println("   Welcome to Book My Stay Application");
+        System.out.println("   Hotel Booking System v1.0");
+        System.out.println("=======================================");
+
+        // Application end message
+        System.out.println("Application started successfully.");
+        System.out.println("Thank you for using our system!");
+    }
+}
